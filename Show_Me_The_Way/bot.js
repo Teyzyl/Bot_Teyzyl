2d4a18efaeb0934d54ac30b5c3c6131c9de2b261
Teyzyl <38924667+Teyzyl@users.noreply.github.com>
2018-05-14 21:14:15 +0200

    Update bot.js

--- Show_Me_The_Way/bot.js
+++ Show_Me_The_Way/bot.js
@@ -148,6 +148,10 @@ bot.on('message', function (message){
     message.channel.send(cadreJoke);
   }
 
+  if (message.content === prefix + "denis"){
+    message.channel.sendMessage("!play https://www.youtube.com/watch?v=Ri7GzCUTC5s");
+  }
+
   if ((message.content === prefix + "help") || (message.content === prefix + "commande") || (message.content === prefix + "commandes")){
     var cadreHelp = new Discord.RichEmbed()
       .setColor("#723AE2")
